c198889d88f7e361889a38aa231270ebcb61732f
David Whiteside <starboarder2001@gmail.com>
2016-09-16 22:06:45 -0600

    added c module http post code

--- pam_pivportal/pam_pivportal.c
+++ pam_pivportal/pam_pivportal.c
@@ -4,6 +4,42 @@
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
+#include <stdio.h>
+#include <curl/curl.h>
+ 
+
+int authenticate_pivportal(char *url, char *username)
+{
+  CURL *curl;
+  CURLcode res;
+  char post_fields[255] = {}
+
+  // Build Post
+  memset(post_fields, 0, sizeof(post_fields));
+  snprintf(post_fields, sizeof(post_fields), "username=%s", username);
+ 
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  curl = curl_easy_init();
+
+  if (curl) {
+      curl_easy_setopt(curl, CURLOPT_URL, url);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+ 
+      res = curl_easy_perform(curl);
+
+      if (res != CURLE_OK) {
+          return 1;
+      }
+ 
+      curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
+
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
     return PAM_SUCCESS;
@@ -17,15 +53,19 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
     int retval;
-
     const char* pUsername;
-    retval = pam_get_user(pamh, &pUsername, "Username: ");
 
-    printf("Welcome %s\n", pUsername);
+    retval = pam_get_user(pamh, &pUsername, "Username: ");
 
     if (retval != PAM_SUCCESS) {
         return retval;
-        //return PAM_AUTH_ERR;
+    }
+
+    // TODO: url should be loaded from a config file
+    retval = authenticate_pivportal(pUsername, "https://127.0.0.1");
+
+    if (retval != 0) {
+        return PAM_AUTH_ERR;
     }
 
     return PAM_SUCCESS;
