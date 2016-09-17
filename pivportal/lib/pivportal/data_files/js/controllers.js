159ff19777dd6735a81635f829330a10f5807f7f
David Whiteside <starboarder2001@gmail.com>
2016-09-17 14:28:52 -0600

    auto refresh page every 5 seconds

--- pivportal/lib/pivportal/data_files/js/controllers.js
+++ pivportal/lib/pivportal/data_files/js/controllers.js
@@ -1,7 +1,7 @@
 var pivportalControllers = angular.module('pivportalControllers', []);
 
-pivportalControllers.controller('pivportalAuthCtrl', ['$auth', '$scope', '$http', '$rootScope', "toaster",
-  function ($auth, $scope, $http, $rootScope, toaster) {
+pivportalControllers.controller('pivportalAuthCtrl', ['$auth', '$scope', '$http', '$rootScope', "toaster", "$interval",
+  function ($auth, $scope, $http, $rootScope, toaster, $interval) {
       $scope.authorize_request = function(username, requestid, client_ip, authorized) {
             // Somehow post data
               $http.post('/api/request/auth', {"username": username, "requestid": requestid, "client_ip": client_ip, "authorized": authorized}).success(function (data) {
@@ -10,10 +10,12 @@ pivportalControllers.controller('pivportalAuthCtrl', ['$auth', '$scope', '$http'
           });
       };
 
-      $http.post('/api/request/list', {"username": "whitesid"}).success(function (data) {
-          $scope.auth_requests = data;
-      }).error(function (data) {
-          console.log(data);
-      });
+      $interval(function(){
+          $http.post('/api/request/list', {"username": "whitesid"}).success(function (data) {
+              $scope.auth_requests = data;
+          }).error(function (data) {
+              console.log(data);
+          });
+      }, 5000);
   }
 ]);
