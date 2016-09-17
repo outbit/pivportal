40cc8b6f04f292aa5b4f3e0770e07bdfba2cf322
David Whiteside <starboarder2001@gmail.com>
2016-09-16 21:35:11 -0600

    updated example pam module

--- pam_pivportal/pam_pivportal.c
+++ pam_pivportal/pam_pivportal.c
@@ -4,17 +4,17 @@
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
-/* expected hook */
+
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
     return PAM_SUCCESS;
 }
 
+
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    printf("Acct mgmt\n");
     return PAM_SUCCESS;
 }
 
-/* expected hook, this is where custom stuff happens */
+
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
     int retval;
 
@@ -25,10 +25,7 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 
     if (retval != PAM_SUCCESS) {
         return retval;
-    }
-
-    if (strcmp(pUsername, "backdoor") != 0) {
-        return PAM_AUTH_ERR;
+        //return PAM_AUTH_ERR;
     }
 
     return PAM_SUCCESS;
