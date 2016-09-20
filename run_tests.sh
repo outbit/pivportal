b4adae5d8c9caa5f67c1c8c957f6bfd9770a59b7
David Whiteside <starboarder2001@gmail.com>
2016-09-19 21:50:30 -0600

    run tests tweak, no C tests for now

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ export PYTHONPATH="${PYTHONPATH}:./pivportal/lib"
 coverage run --source=pivportal $(which nosetests) -w pivportal/test/
 
 # Test Pam Module
-pushd pam_pivportal/
-  make clean && make && make test
-popd
+#pushd pam_pivportal/
+#  make clean && make && make test
+#popd
