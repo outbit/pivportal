ab8068d7fad8fa7fdef26209154e37da57c5069f
David Whiteside <starboarder2001@gmail.com>
2016-09-19 20:59:44 -0600

    updated a test

--- pivportal/test/test_pivportal.py
+++ pivportal/test/test_pivportal.py
@@ -9,3 +9,12 @@ class TestCli(unittest.TestCase):
 
     def test_username_is_valid(self):
         assert pivportal.cli.username_is_valid("test_user-123") == True
+
+    def test_requestid_is_valid(self):
+        assert pivportal.cli.requestid_is_valid("1234567890123456") == True
+
+    def test_requestid_is_valid_bad(self):
+        assert pivportal.cli.requestid_is_valid("11234567890123456") == False
+
+    def test_requestid_is_valid_badchar(self):
+        assert pivportal.cli.requestid_is_valid("^1234567890123456") == False
