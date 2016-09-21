8eb459fd925cf9d4cb2cc8ab4dc310f4abd7924c
David Whiteside <starboarder2001@gmail.com>
2016-09-20 20:56:26 -0600

    fixed #2. pivportal now supports a config file and allow specifying authorized users

--- pivportal/lib/pivportal/cli.py
+++ pivportal/lib/pivportal/cli.py
@@ -5,6 +5,7 @@ from random import choice
 from string import ascii_uppercase
 import re
 import json
+import yaml
 
 
 app = Flask(__name__)
@@ -162,19 +163,41 @@ class Cli(object):
         parser.add_option("-p", "--port", dest="port",
                               help="port",
                               metavar="PORT",
-                              default="8088")
+                              default=None)
         parser.add_option("-l", "--listen", dest="listen",
                               help="Listen IP Address",
                               metavar="LISTEN",
-                              default="127.0.0.1")
+                              default=None)
         parser.add_option("-d", "--debug", dest="debug",
                               help="debug",
                               metavar="DEBUG",
-                              default=False)
+                              default=None)
         (options, args) = parser.parse_args()
         self.port = options.port
         self.listen = options.listen
         self.is_debug = options.debug
+
+        # Set Defaults If Not Set On CLI
+        if self.port is None:
+            self.port = "8088"
+        if self.listen is None:
+            self.listen = "127.0.0.1"
+        if self.is_debug is None:
+            self.is_debug = False
+
+        # Load Configurations if not set on CLI
+        if os.path.isfile('/etc/pivportal-server.conf'):
+            with open('/etc/pivportal-server.conf', 'r') as f:
+                pivportal_conf = yaml.load(f)
+                if "authorized_users" in pivportal_conf:
+                    dn_to_username = dict(pivportal_confg["authorized_users"])
+                if "port" in pivportal_conf:
+                    if self.port is None:
+                        self.port = str(pivportal_conf["port"])
+                if "listen_address" in pivportal_conf:
+                    if self.listen is None:
+                        self.listen = str(pivportal_conf["listen_address"])
+
 
     def run(self):
         """ EntryPoint Of Application """
