4e3cf234a5f2e56164b075a03c96e2c843859842
David Whiteside <starboarder2001@gmail.com>
2021-02-05 21:10:59 -0700

    maybe?

--- pivportal/lib/pivportal/security.py
+++ pivportal/lib/pivportal/security.py
@@ -58,11 +58,11 @@ def create_token(user, secret_key):
     }
 
     token = jwt.encode(payload, secret_key, algorithm='HS256')
-    return token.encode().decode('unicode_escape')
+    return token
 
 
 def parse_token(token, secret_key):
-    return jwt.encode().decode(token, secret_key, algorithms='HS256')
+    return jwt.decode(token, secret_key, algorithms='HS256')
 
 
 def token_required(secret_key):
